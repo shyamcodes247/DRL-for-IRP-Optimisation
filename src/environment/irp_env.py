e2f2d9813e80f98f48c1e9fdd52acdf86f037519
shyamcodes247 <shyamamin9569@gmail.com>
2026-08-07 10:14:01 +0800

    Added some attributes to environment object.

--- src/environment/irp_env.py
+++ src/environment/irp_env.py
@@ -2,22 +2,34 @@ import numpy as np
 import gymnasium as gym
 
 class IRPEnv(gym.Env):
-    def __init__(self):
+    def __init__(self, episode_length, inventory_capacity, num_nodes, loc_dim):
+        self.episode_length = episode_length
+        self.current_step = 0
+        self.inventory_capacity = inventory_capacity
+        self.inventory_observation_space = gym.spaces.Dict(
+            {
+                "location": gym.spaces.Box(low=..., high=..., shape=(num_nodes, loc_dim), dtype=np.float32),
+                "current_inventory": gym.spaces.Box(low=0, high=inventory_capacity, shape=(num_nodes,), dtype=np.float32),
+                "replenishment_history": gym.spaces.Box(low=0, high=np.inf, shape=(num_nodes, ), dtype=np.float32),
+                "historical_demands": gym.spaces.Box(low=0, high=np.inf, shape=(num_nodes,), dtype=np.float32)
+            }
+        )
+        self.inventory_action_space
+        self.routing_observation_space
+        self.routing_action_space
+        self.critic_observation_space
+        self.critic_action_space
+        
         pass
 
     def reset(self):
         pass
 
-    def step(self, action):
+    def inventory_action_step(self, action):
+        pass
+    
+    def routing_action_step(self, action):
         pass
         
     def render(self):
-        pass
-
-    @staticmethod
-    def observation_space(self):
-        pass
-    
-    @staticmethod
-    def action_space(self):
         pass
