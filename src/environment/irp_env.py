c239f4595a19d5553da88c1ca92f456eb72a0a68
shyamcodes247 <shyamamin9569@gmail.com>
2026-08-08 15:34:55 +0800

    Finished first implementation of __init_ method for environment class.

--- src/environment/irp_env.py
+++ src/environment/irp_env.py
@@ -2,25 +2,51 @@ import numpy as np
 import gymnasium as gym
 
 class IRPEnv(gym.Env):
-    def __init__(self, episode_length, inventory_capacity, num_nodes, loc_dim):
+    def __init__(self, episode_length, inventory_capacity, num_retailers, loc_dim, lookback_window, max_demand, loading_capacity, adjacency_list):
         self.episode_length = episode_length
         self.current_step = 0
         self.inventory_capacity = inventory_capacity
+        self.num_retailers = num_retailers
+        self.loc_dim = loc_dim
+        self.lookback_window = lookback_window
+        self.max_demand = max_demand
+        self.loading_capacity = loading_capacity
+        self.adjacency_list = adjacency_list
         self.inventory_observation_space = gym.spaces.Dict(
             {
-                "location": gym.spaces.Box(low=..., high=..., shape=(num_nodes, loc_dim), dtype=np.float32),
-                "current_inventory": gym.spaces.Box(low=0, high=inventory_capacity, shape=(num_nodes,), dtype=np.float32),
-                "replenishment_history": gym.spaces.Box(low=0, high=np.inf, shape=(num_nodes, ), dtype=np.float32),
-                "historical_demands": gym.spaces.Box(low=0, high=np.inf, shape=(num_nodes,), dtype=np.float32)
+                "location": gym.spaces.Box(low=..., high=..., shape=(num_retailers, loc_dim), dtype=np.float32),
+                "current_inventory": gym.spaces.Box(low=0, high=inventory_capacity, shape=(num_retailers,), dtype=np.float32),
+                "current_demand": gym.spaces.Box(low=0, high=max_demand, shape=(num_retailers,), dtype=np.float32),
+                "replenishment_history": gym.spaces.Box(low=0, high=inventory_capacity, shape=(num_retailers, lookback_window), dtype=np.float32),
+                "historical_demands": gym.spaces.Box(low=0, high=max_demand, shape=(num_retailers, lookback_window), dtype=np.float32)
             }
         )
-        self.inventory_action_space
-        self.routing_observation_space
-        self.routing_action_space
-        self.critic_observation_space
-        self.critic_action_space
-        
-        pass
+        self.inventory_action_space =  gym.spaces.Box(low=0, high=inventory_capacity, shape=(num_retailers,), dtype=np.float32)
+        self.routing_observation_space = gym.spaces.Dict(
+            {
+                "location": gym.spaces.Box(low=..., high=..., shape=(num_retailers + 1, loc_dim), dtype=np.float32),
+                "vehicle_position": gym.spaces.Discrete(num_retailers + 1),
+                "replenishment_amount": gym.spaces.Box(low=0, high=np.inf, shape=(num_retailers, ), dtype=np.float32),
+                "current_load_capacity": gym.spaces.Box(low=0, high=loading_capacity, shape=(1,), dtype=np.float32),
+                "visited_mask": gym.spaces.MultiBinary(num_retailers + 1)
+            }
+        )
+        self.routing_action_space = gym.spaces.Discrete(num_retailers + 1)
+        self.critic_observation_space = gym.spaces.Dict(
+            {
+                "location": gym.spaces.Box(low=..., high=..., shape=(num_retailers, loc_dim), dtype=np.float32),
+
+                # Inventory-side info — from inventory_observation_space
+                "current_inventory": gym.spaces.Box(low=0, high=inventory_capacity, shape=(num_retailers,), dtype=np.float32),
+                "replenishment_history": gym.spaces.Box(low=0, high=inventory_capacity, shape=(num_retailers, lookback_window), dtype=np.float32),
+                "historical_demands": gym.spaces.Box(low=0, high=max_demand, shape=(num_retailers, lookback_window), dtype=np.float32),
+
+                # Routing-side info — from routing_observation_space
+                "current_load_capacity": gym.spaces.Box(low=0, high=loading_capacity, shape=(1,), dtype=np.float32),
+                "visited_mask": gym.spaces.MultiBinary(num_retailers + 1),
+                "vehicle_position": gym.spaces.Discrete(num_retailers + 1),
+            }
+        )
 
     def reset(self):
         pass
