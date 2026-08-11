9b8e7ab08f12ea4303ef950fc2f58857e34edaf5
shyamcodes247 <shyamamin9569@gmail.com>
2026-08-11 11:19:27 +0800

    Implemented a data converter script to convert data from files into a data structure.

---
+++ src/environment/data_converter.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+# might need to reference as AI-generated
+def convert_instance(path):
+    with open(path, "r") as f:
+        lines = [line.strip().split('\t') for line in f if line.strip()]
+
+    num_nodes, episode_length, vehicle_capacity = lines[0]
+    parameters = {
+        "num_nodes": int(num_nodes),
+        "episode_length": int(episode_length),
+        "vehicle_capacity": float(vehicle_capacity)
+
+    }
+    supplier_info = lines[1]
+    supplier = {
+        "id": int(supplier_info[0]),
+        "x_cord": float(supplier_info[1]),
+        "y_cord": float(supplier_info[2]),
+        "initial_inventory" : float(supplier_info[3]),
+        "production rate": float(supplier_info[4]),
+        "holding_cost": float(supplier_info[5])
+    }
+
+    retailers_info = lines[2:]
+    retailers = pd.DataFrame(retailers_info, columns=["id", "x_cord", "y_cord", "initial_inventory", "max_capacity", "min_capacity", "demand", "holding_cost"]).astype(float)
+    retailers["id"] = retailers["id"].astype(int)
+
+    return {"parameters": parameters, "supplier": supplier, "retailers": retailers}
