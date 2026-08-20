223a16325c3ff0ea87132d3c559360f089a898b1
shyamcodes247 <shyamamin9569@gmail.com>
2026-08-20 13:02:49 +0800

    Deleted unneccessary networks folder and moved files to agent folder.

---
+++ src/agent/critic.py
@@ -0,0 +1,8 @@
+import torch 
+
+class Critic():
+    def __init__(self):
+        pass
+
+
+
---
+++ src/agent/features.py
@@ -0,0 +1,25 @@
+import torch
+import numpy as np
+
+def build_critic_features(obs):
+    loc = obs["location"]
+    curr_inv = obs["current_inventory"][:, None]
+    past_replenishment = obs["replenishment_history"][:, -1:]
+    holding_cost = obs["holding_cost"][:, None]
+    historical_demands = obs["historical_demands"][:, -1:]
+    features = np.hstack([loc, curr_inv, past_replenishment, holding_cost, historical_demands])
+    return torch.from_numpy(features).float()
+
+def build_inventory_features(obs):
+    loc = obs["location"]
+    curr_inv = obs["current_inventory"][:, None]
+    past_replenishment = obs["replenishment_history"][:, -1:]
+    curr_demand = obs["historical_demands"][:, -1:]
+    features = np.hstack([loc, curr_inv, past_replenishment, curr_demand])
+    return torch.from_numpy(features).float()
+
+def build_routing_features(obs):
+    loc = obs["location"]
+    replenishment = np.concatenate([[0.0], obs["replenishment_amount"]])
+    features = np.hstack([loc, replenishment[:, None]])
+    return torch.from_numpy(features).float()
